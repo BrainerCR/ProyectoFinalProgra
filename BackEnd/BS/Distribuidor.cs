3b9b51ff2113ac3bc5f76826fb3de758a6e360b8
BrainerCR <63078436+BrainerCR@users.noreply.github.com>
2022-03-31 23:05:07 -0600

    clase VinculoProducto en DAL

---
+++ BackEnd/BS/Distribuidor.cs
@@ -0,0 +1,55 @@
+﻿using DAL.DO.Interfaces;
+using DAL.EF;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using data = DAL.DO.Objects;
+
+namespace BS
+{
+    public class Distribuidor : ICRUD<data.Distribuidor>
+    {
+        private NDbContex context;
+
+        public Distribuidor(NDbContex _context)
+        {
+            context = _context;
+        }
+
+        public void Delete(data.Distribuidor t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<data.Distribuidor> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<data.Distribuidor>> GetAllWithAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public data.Distribuidor GetOneByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<data.Distribuidor> GetOneByIdWithAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(data.Distribuidor t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(data.Distribuidor t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- BackEnd/BS/VinculoProducto.cs
+++ BackEnd/BS/VinculoProducto.cs
@@ -1,10 +1,54 @@
-﻿using System;
+﻿using DAL.DO.Interfaces;
+using DAL.EF;
+using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Threading.Tasks;
+using data = DAL.DO.Objects;
 
-namespace DAL
+namespace BS
 {
-    class VinculoProducto
+    public class VinculoProducto : ICRUD<data.VinculoProducto>
     {
+        private NDbContex context;
+
+        public VinculoProducto(NDbContex _context)
+        {
+            context = _context;
+        }
+
+        public void Delete(data.VinculoProducto t)
+        {
+            new DAL.VinculoProducto(context).Delete(t);
+        }
+
+        public IEnumerable<data.VinculoProducto> GetAll()
+        {
+            return new DAL.VinculoProducto(context).GetAll();
+        }
+
+        public async Task<IEnumerable<data.VinculoProducto>> GetAllWithAsync()
+        {
+            return await new DAL.VinculoProducto(context).GetAllWithAsync();
+        }
+
+        public data.VinculoProducto GetOneByID(int id)
+        {
+            return new DAL.VinculoProducto(context).GetOneByID(id);
+        }
+
+        public Task<data.VinculoProducto> GetOneByIdWithAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(data.VinculoProducto t)
+        {
+            new DAL.VinculoProducto(context).Insert(t);
+        }
+
+        public void Update(data.VinculoProducto t)
+        {
+            new DAL.VinculoProducto(context).Update(t);
+        }
     }
 }
