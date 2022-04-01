51434f2dbb8ddb62b96fe5a832fea30ff64de94e
BrainerCR <63078436+BrainerCR@users.noreply.github.com>
2022-03-31 21:06:56 -0600

    Se agrego la clase Producto en DAL.DO.Object

--- BackEnd/DAL.DO/Objects/Producto.cs
+++ BackEnd/DAL.DO/Objects/Producto.cs
@@ -4,7 +4,19 @@ using System.Text;
 
 namespace DAL.DO.Objects
 {
-    class Producto
+    public partial class Producto
     {
+        public int IdProducto { get; set; }
+        public DateTime FechaIngreso { get; set; }
+        public string NombreProducto { get; set; }
+        public int CantidadProducto { get; set; }
+        public int PrecioUnitario { get; set; }
+        public int PrecioVenta { get; set; }
+        public int IdVinculoProducto { get; set; }
+        public int IdDistribuidor { get; set; }
+
+        //Descomentar cuando se tenga creado sus clases respectivas
+        //public virtual Distribuidor IdDistribuidorNavigation { get; set; }
+        //public virtual VinculoProducto IdVinculoProductoNavigation { get; set; }
     }
 }
