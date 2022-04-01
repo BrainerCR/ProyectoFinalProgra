1a208537ce276b6c699a79a733cf91ec1b7ee659
BrainerCR <63078436+BrainerCR@users.noreply.github.com>
2022-03-31 22:07:26 -0600

    clase VinculoProducto en DAL

--- BackEnd/DAL/VinculoProducto.cs
+++ BackEnd/DAL/VinculoProducto.cs
@@ -1,10 +1,58 @@
-﻿using System;
+﻿using DAL.DO.Interfaces;
+using DAL.EF;
+using DAL.Repository;
+using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Threading.Tasks;
+using data = DAL.DO.Objects;
 
-namespace DAL.DO.Objects
+namespace DAL
 {
-    class VinculoProducto
+    public class VinculoProducto : ICRUD<data.VinculoProducto>
     {
+        private Repository<data.VinculoProducto> _repo = null;
+
+        public VinculoProducto(NDbContex DbContext)
+        {
+            _repo = new Repository<data.VinculoProducto>(DbContext);
+        }
+
+        public void Delete(data.VinculoProducto t)
+        {
+            _repo.Delete(t);
+            _repo.Commit();
+        }
+
+        public IEnumerable<data.VinculoProducto> GetAll()
+        {
+            return _repo.GetAll();
+        }
+
+        public Task<IEnumerable<data.VinculoProducto>> GetAllWithAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public data.VinculoProducto GetOneByID(int id)
+        {
+            return _repo.GetOneById(id);
+        }
+
+        public Task<data.VinculoProducto> GetOneByIdWithAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(data.VinculoProducto t)
+        {
+            _repo.Insert(t);
+            _repo.Commit();
+        }
+
+        public void Update(data.VinculoProducto t)
+        {
+            _repo.Update(t);
+            _repo.Commit();
+        }
     }
 }
