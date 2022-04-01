09a1841b2f79f3969c59fa8d1014e0b9ad154589
BrainerCR <63078436+BrainerCR@users.noreply.github.com>
2022-03-31 21:45:34 -0600

    se agregó la clase producto en DAL

--- BackEnd/DAL/Producto.cs
+++ BackEnd/DAL/Producto.cs
@@ -1,10 +1,57 @@
-﻿using System;
+﻿using DAL.DO.Interfaces;
+using DAL.EF;
+using DAL.Repository;
 using System.Collections.Generic;
-using System.Text;
+using System.Threading.Tasks;
+using data = DAL.DO.Objects;
 
-namespace DAL.DO.Objects
+namespace DAL
 {
-    class Producto
+    public class Producto : ICRUD<data.Producto>
     {
+        private RepositoryProducto _repo = null;
+
+        public Producto(NDbContex DbContext)
+        {
+            _repo = new RepositoryProducto(DbContext);
+        }
+
+        public void Delete(data.Producto t)
+        {
+            _repo.Delete(t);
+            _repo.Commit();
+        }
+
+        public IEnumerable<data.Producto> GetAll()
+        {
+            return _repo.GetAll();
+        }
+
+        public async Task<IEnumerable<data.Producto>> GetAllWithAsync()
+        {
+            return await _repo.GetAllWithAsAsync();
+        }
+
+        public data.Producto GetOneByID(int id)
+        {
+            return _repo.GetOneById(id);
+        }
+
+        public async Task<data.Producto> GetOneByIdWithAsync(int id)
+        {
+            return await _repo.GetOneByIdAsAsync(id);
+        }
+
+        public void Insert(data.Producto t)
+        {
+            _repo.Insert(t);
+            _repo.Commit();
+        }
+
+        public void Update(data.Producto t)
+        {
+            _repo.Update(t);
+            _repo.Commit();
+        }
     }
 }
