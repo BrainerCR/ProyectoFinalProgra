d4da82b8066e056d83d93bbcb361fbaf5516a0e0
BrainerCR <63078436+BrainerCR@users.noreply.github.com>
2022-04-18 13:48:06 -0600

    se agrego los data model faltantes y actualización del mappingprofile

--- BackEnd/API/Mapping/MappingProfile.cs
+++ BackEnd/API/Mapping/MappingProfile.cs
@@ -10,6 +10,10 @@ namespace API.Mapping
             CreateMap<data.Empleado, DataModels.Empleado>().ReverseMap();
             CreateMap<data.RolEmpleado, DataModels.RolEmpleado>().ReverseMap();
             CreateMap<data.RegistroFactura, DataModels.RegistroFactura>().ReverseMap();
+            CreateMap<data.Cliente, DataModels.Cliente>().ReverseMap();
+            CreateMap<data.Producto, DataModels.Producto>().ReverseMap();
+            CreateMap<data.Distribuidor, DataModels.Distribuidor>().ReverseMap();
+            CreateMap<data.VinculoProducto, DataModels.VinculoProducto>().ReverseMap();
         }
     }
 }
--- BackEnd/BS/Cliente.cs
+++ BackEnd/BS/Cliente.cs
@@ -1,10 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Threading.Tasks;
+using DAL.DO.Interfaces;
+using DAL.EF;
+using data = DAL.DO.Objects;
 
 namespace DAL
 {
-    class Cliente
+    public class Cliente : ICRUD<data.Cliente>
     {
+        private NDbContex context;
+
+        public Cliente(NDbContex _context)
+        {
+            context = _context;
+        }
+
+        public void Delete(data.Cliente t)
+        {
+            new DAL.Cliente(context).Delete(t); ;
+        }
+
+        public IEnumerable<data.Cliente> GetAll()
+        {
+            return new DAL.Cliente(context).GetAll();
+        }
+
+        public async Task<IEnumerable<data.Cliente>> GetAllWithAsync()
+        {
+            return await new DAL.Cliente(context).GetAllWithAsync();
+        }
+
+        public data.Cliente GetOneByID(int id)
+        {
+            return new DAL.Cliente(context).GetOneByID(id);
+        }
+
+        public Task<data.Cliente> GetOneByIdWithAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(data.Cliente t)
+        {
+            new DAL.Cliente(context).Insert(t);
+        }
+
+        public void Update(data.Cliente t)
+        {
+            new DAL.Cliente(context).Update(t);
+        }
     }
 }
